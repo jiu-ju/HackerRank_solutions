48ccd0506163045e691b62d1d95b06997d0eb008
jiu-ju <57570110+DzhulAleks@users.noreply.github.com>
2020-10-23 14:58:35 -0500

    Day0

--- 30_Days_of_Code/src/main/java/Day0_HelloWorld.java
+++ 30_Days_of_Code/src/main/java/Day0_HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HelloWorld {
+public class Day0_HelloWorld {
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
         Scanner scan = new Scanner(System.in);
