513a9ad9267045c4d721f1c2a131bb679a50ae71
jiu-ju <57570110+DzhulAleks@users.noreply.github.com>
2020-10-23 14:49:39 -0500

    Day1

---
+++ 30_Days_of_Code/src/main/java/DataTypes.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double b;
+        String c;
+        /* Read and save an integer, double, and String to your variables.*/
+        a = scan.nextInt();
+        b = scan.nextDouble();
+        scan.nextLine();
+        c = scan.nextLine();
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + a);
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d + b);
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        System.out.println(s + c);
+        scan.close();
+    }
+}
+/*
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+ */
+
